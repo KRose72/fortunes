5c33b5fd5c03936d3097d064fc13175c8b78a4ff
RoseK <RoseK@uavdit01dnp0002.GMCC.GRANGE.LOCAL>
2022-07-22 11:36:09 -0400

    First Commit

---
+++ src/main/java/com/grangeinsurance/fortunes/FortunesApplication.java
@@ -0,0 +1,13 @@
+package com.grangeinsurance.fortunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortunesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grangeinsurance/fortunes/FortunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grangeinsurance.fortunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
