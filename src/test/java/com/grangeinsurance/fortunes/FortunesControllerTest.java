d79b4ab6e629870153128251b0b46d2d5fbc8479
RoseK <RoseK@uavdit01dnp0004.GMCC.GRANGE.LOCAL>
2022-07-26 12:36:19 -0400

    Cleaned SonarLint

--- src/test/java/com/grangeinsurance/fortunes/FortunesControllerTest.java
+++ src/test/java/com/grangeinsurance/fortunes/FortunesControllerTest.java
@@ -13,7 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class FortunesControllerTest {
+class FortunesControllerTest {
 
 	@Mock FortunesService mockService;
 	
--- src/test/java/com/grangeinsurance/fortunes/FortunesServiceTest.java
+++ src/test/java/com/grangeinsurance/fortunes/FortunesServiceTest.java
@@ -13,7 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class FortunesServiceTest {
+class FortunesServiceTest {
 	
 	@Mock FortunesList mockService;
 	
